aa44166154bffe6a566e0b6bad887b211dd35a9c
ljv5555 <jjverderber@gmail.com>
2014-07-01 19:16:59 -0500

    Update cssplot.js

--- cssplot.js
+++ cssplot.js
@@ -1,12 +1,51 @@
 /** cssplot.js **/
 function createCssPlot(elemen_in,xsize_in,ysize_in)
 {
+  
+  
   var rtn = function(element,xsize,ysize)
   {
-    
+    this.uid = (new Date()).getTime()+'_'+Math.round(100000*Math.random());
+    this.element = element;
+    this.css0 = document.createElement('style');
+    this.css0.className="css0_"+this.uid;
+    this.css0.innerHTML='.table_'+this.uid+' td{width:1px;height:1px;}'
+    this.element.appendChild(this.css0);
+    this.css = document.createElement('style');
+    this.css.className="css_"+this.uid;
+    this.element.appendChild(this.css);
+    this.table = document.createElement('table');
+    this.table.className="table_"+this.uid;
+    for(var i = 0;i<ysize;i++)
+    {
+      var row = document.createElement('tr');
+      row.className='tr_'+uid+'_'+i;
+      for(var j=0;j<xsize;j++)
+      {
+        var cell = document.createElement('td');
+        cell.className='td_'+uid+'_'+j;
+      }
+      this.table.appendChild(row);
+      
+    }
+    this.element.appendChild(this.table);
+    var newcss = this.css;
+    var newuid = this.uid;
+    this.updatePlot=function(data,offset)
+    {
+      var rtn = '';
+      if(!offset){offset=0;}
+      for(var k=0;k<data;k++)
+      {
+        var tri = offset + data[k];
+        var tdi = k;
+        rtn+='\n .tr_'+newuid+'_'+tdi+' ';
+        rtn+= ' .td_'+newuid+'_'+tri+'{ background:black;} \n';
+      }
+      newcss.innerHTML=rtn;
+    };
     this.ysize=ysize;
     this.xsize=xsize;
-    this.element=element;
   };
   return new rtn(element_in,xsize_in,ysize_in);
 }
