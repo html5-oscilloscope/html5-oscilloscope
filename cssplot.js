c77a0b6187fa847028c681e83529500a4ffc4e0b
ljv5555 <jjverderber@gmail.com>
2014-07-01 19:46:17 -0500

    Update cssplot.js

--- cssplot.js
+++ cssplot.js
@@ -1,11 +1,12 @@
 /** cssplot.js **/
-function createCssPlot(elemen_in,xsize_in,ysize_in)
+function createCssPlot(element_in,xsize_in,ysize_in)
 {
   
   
   var rtn = function(element,xsize,ysize)
   {
     this.uid = (new Date()).getTime()+'_'+Math.round(100000*Math.random());
+    var uid = this.uid;
     this.element = element;
     this.css0 = document.createElement('style');
     this.css0.className="css0_"+this.uid;
@@ -24,6 +25,7 @@ function createCssPlot(elemen_in,xsize_in,ysize_in)
       {
         var cell = document.createElement('td');
         cell.className='td_'+uid+'_'+j;
+        row.appendChild(cell);
       }
       this.table.appendChild(row);
       
@@ -35,14 +37,14 @@ function createCssPlot(elemen_in,xsize_in,ysize_in)
     {
       var rtn = '';
       if(!offset){offset=0;}
-      for(var k=0;k<data;k++)
+      for(var k=0;k<data.length;k++)
       {
         var tri = offset + data[k];
         var tdi = k;
         rtn+='\n .tr_'+newuid+'_'+tdi+' ';
         rtn+= ' .td_'+newuid+'_'+tri+'{ background:black;} \n';
       }
-      newcss.innerHTML=rtn;
+      newcss.innerHTML=rtn;console.log('rtn:');console.log(rtn);
     };
     this.ysize=ysize;
     this.xsize=xsize;
