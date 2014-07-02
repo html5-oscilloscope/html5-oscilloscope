e51c8b8eda9dbc272b67a412bc383bd8a3ce6231
ljv5555 <jjverderber@gmail.com>
2014-07-01 20:02:44 -0500

    Update cssplot.js

--- cssplot.js
+++ cssplot.js
@@ -10,7 +10,8 @@ function createCssPlot(element_in,xsize_in,ysize_in)
     this.element = element;
     this.css0 = document.createElement('style');
     this.css0.className="css0_"+this.uid;
-    this.css0.innerHTML='.table_'+this.uid+' td{width:1px;height:1px;}'
+    this.css0.innerHTML='\n .table_'+this.uid+'{ padding:0px;margin:0px;border-spacing: 0px; }'
+                        +' \n .table_'+this.uid+' td{width:1px;height:1px;}';
     this.element.appendChild(this.css0);
     this.css = document.createElement('style');
     this.css.className="css_"+this.uid;
@@ -20,11 +21,11 @@ function createCssPlot(element_in,xsize_in,ysize_in)
     for(var i = 0;i<ysize;i++)
     {
       var row = document.createElement('tr');
-      row.className='tr_'+uid+'_'+i;
+      row.className='tr_'+this.uid+'_'+i;
       for(var j=0;j<xsize;j++)
       {
         var cell = document.createElement('td');
-        cell.className='td_'+uid+'_'+j;
+        cell.className='td_'+this.uid+'_'+j;
         row.appendChild(cell);
       }
       this.table.appendChild(row);
@@ -33,18 +34,27 @@ function createCssPlot(element_in,xsize_in,ysize_in)
     this.element.appendChild(this.table);
     var newcss = this.css;
     var newuid = this.uid;
-    this.updatePlot=function(data,offset)
+    this.updatePlot=function(data,offset,keepprevious,bgcolor)
     {
+      if(!keepprevious){keepprevious=false;}
+      if(!bgcolor){bgcolor='grey';}
       var rtn = '';
       if(!offset){offset=0;}
       for(var k=0;k<data.length;k++)
       {
         var tri = offset + data[k];
         var tdi = k;
-        rtn+='\n .tr_'+newuid+'_'+tdi+' ';
-        rtn+= ' .td_'+newuid+'_'+tri+'{ background:black;} \n';
+        rtn+='\n .tr_'+newuid+'_'+tri+' ';
+        rtn+= ' .td_'+newuid+'_'+tdi+'{ background:'+bgcolor+';} \n';
       }
-      newcss.innerHTML=rtn;console.log('rtn:');console.log(rtn);
+      if(!keepprevious)
+      {
+        newcss.innerHTML=rtn;
+      }
+      else
+      {
+        newcss.innerHTML+='\n'+rtn;
+      }
     };
     this.ysize=ysize;
     this.xsize=xsize;
